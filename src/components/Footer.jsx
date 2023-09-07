23b175a5fccc380269f96245239c39dd8583d539
divanshu31 <90476277+divanshu31@users.noreply.github.com>
2023-09-07 22:44:21 +0530

    Footer Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,7 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://divanshu31.github.io/Portfolio/" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Divanshu Manchanda</a>
-            </p>
+
             <a className="text-dark fs-4" href="https://github.com/divanshu31" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
